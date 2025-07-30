bcecbc917e503365cb4a3b0aabacfe57a8918329
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2025-07-30 20:57:08 +0530

    divisible by 2 added

--- basics.cpp
+++ basics.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int age;
-    cout << "Enter your age:";
-    cin >> age;
+// int main(){
+//     int age;
+//     cout << "Enter your age:";
+//     cin >> age;
 
-    if(age>=18){
-        cout << "You can Vote";
+//     if(age>=18){
+//         cout << "You can Vote";
+//     }else{
+//         cout << "You can't Vote";
+//     }
+//     return 0;
+// }
+
+int main(){
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    if(n%2==0){
+        cout << "Even number";
     }else{
-        cout << "You can't Vote";
+        cout <<"Odd number";
     }
-    return 0;
 }
