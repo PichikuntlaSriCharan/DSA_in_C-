19533970908eb817ebd2d1c378525dcb1f268c40
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2025-08-02 08:37:23 +0530

    Loops completed in dsa

--- basics.cpp
+++ basics.cpp
@@ -82,9 +82,114 @@ using namespace std;
 // }
 
 //Ternery statement
-int main(){
-    int n=45;
-    cout << (n >= 0 ? "Positive" : "Negative") << endl;
+// int main(){
+//     int n=45;
+//     cout << (n >= 0 ? "Positive" : "Negative") << endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
+// //Practice question 1
+// int main(){
+//     int n=0,sum=0,j;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     j=n;
+//     for(int i=0;i<=n;i++){
+//         sum+=i;
+//         if(i==5){
+//             break;
+//         }
+//     }
+
+//     cout << "Sum of " << j << " numbers is " << sum << endl;
+
+
+//     return 0;
+// }
+
+// int main(){
+//     int n=0,sum=0,j;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     j=n;
+//     for(int i=0;i<=n;i+=2){
+//         sum+=i;
+//     }
+
+//     cout << "Odd Sum of " << j << " odd numbers is " << sum << endl;
+
+
+//     return 0;
+// }
+
+// int main(){
+//     do{
+//         cout << "Hello world";
+//     } while(3>5);
+//     return 0;
+// }
+
+// int main(){
+//     int n,i,j=0;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     for(i=2;i<n;i++){
+//         if(n%i==0){
+//             j++;
+//         }
+//     }
+//     if(j<1){
+//         cout << "Prime Number";
+//     }else{
+//         cout << "Not a prime" << endl;
+//     }
+// }
+
+// int main(){
+//     int n;
+//     bool isPrime = true;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//      for(int i=2;i*i<n;i++){         // i*i<n   or   i<n
+//         if(n%i==0){
+//             isPrime = false;
+//             break;
+//         }
+//      }
+
+//      if(isPrime == true){
+//         cout << "Prime numberr \n";
+//      }else{
+//         cout << "Not Prime number\n";
+//      }
+
+//      return 0;
+// }
+
+//Nested loops
+// int main(){
+//     int n;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     for(int i=1;i<=n;i++){
+//         int m=10;
+//         for(int i=1;i<=m;i++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// If else || else if || ternery
+//for ,while, do-while
+//Break
+//Nested Loops
+
+// int main(){
+//     int n;
+//     cout << "Enter a number : ";
+//     cin >> n;
+//     for(i=0;i<=n;i)
+// }
